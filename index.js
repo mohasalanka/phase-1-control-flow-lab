7b10f11dde93f556be45c01e309df45a2c39c441
Mohamed <mohasalanka2020@gmail.com>
2023-03-14 10:21:57 +0300

    done asignment

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
 
-function ternaryCheckCity(){
-  // Write your code here!
+  function scuberGreetingForFeet(feet){
+  
+  
+  if ( feet < 400){
+    return`This one is on me!`;
+  }else if(feet > 2000 ){
+    if(feet > 2500){
+      return'No can do.'
+    }
+    return 'I will gladly take your thirty bucks.';
+  }
+  
 }
+  
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+
+  console.log(ternaryCheckCity('NYC'))
+
+  function ternaryCheckCity(cityName){
+    let city;
+    return city = cityName === 'NYC' ? ("Ok, sounds good.") : ("No go.") ; 
+    
+  }
+  
+  
+  ternaryCheckCity('NYC')
+  
+  function switchOnCharmFromTip(tip){
+  
+    switch (tip){
+      case 'generous' :
+        return 'Thank you so much.'
+      case 'not as generous':
+        return 'Thank you.'
+      default :
+      return 'Bye.'
+    }
 }
+switchOnCharmFromTip('generous')
